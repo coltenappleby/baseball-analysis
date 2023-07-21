e01dd859fb808e9e2cc8455bd9be0e074a551105
Colten Appleby <colten.appleby@gmail.com>
2023-07-21 16:36:41 -0400

    added additional plots to errors

--- errors-per-game/errors-per-game.py
+++ errors-per-game/errors-per-game.py
@@ -7,6 +7,15 @@ data = pd.read_csv('errors-per-game/year-by-year-averages.csv')
 
 modern = data.query('Year >= 1920')
 
-fig = px.line(modern, x='Year', y='E', title='MLB Errors per Game by Year', markers=True)
+# Error per game by year
+# fig = px.line(modern, x='Year', y='E', title='MLB Errors per Game by Year', markers=True)
+
+# Error per game by year, with 5-year moving average
+# modern['5yr'] = modern['E'].rolling(5).mean()
+fig = px.line(modern, x='Year', y='5yr', title='MLB Errors per Game by Year', markers=True)
+
+# Error/Chances per game by year
+# modern['E/Ch'] = modern['E'] / modern['Ch']
+# fig = px.line(modern, x='Year', y='E/Ch', title='MLB Errors per Chance by Year', markers=True)
 
 fig.show()
